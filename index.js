37f9e40f86eb80bf4529adf4f153e33fa9c9e211
Nick <copinf@mymail.vcu.edu>
2019-03-21 16:44:49 -0400

    Initial commit.

---
+++ index.js
@@ -0,0 +1,30 @@
+const config = require('./config.json');
+const commands = require('./commandPayload.json');
+const fs = require('fs');
+const node_ssh = require('node-ssh');
+ssh = new node_ssh();
+
+ssh.connect({
+	host:config.host,
+	username:config.username,
+	privateKey: config.keyFile
+}).then(()=>{
+	commands.forEach(command=>{
+		ssh.execCommand(command.command,{cwd:'/'}).then((res)=>{
+			let passed = true;
+			if(command.expectValue){
+				if(res.stdout === '') passed = false;
+			}
+
+			if(passed){
+				console.log(`✔️ Test ${command.description} Passed!`);
+			} else {
+				console.log(`✖️ Test ${command.description} Failed!`);
+				
+			}
+		});
+
+	});
+}).catch((e)=>{
+	console.log(e);
+});
